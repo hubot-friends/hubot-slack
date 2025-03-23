21956d13763e18a70235e29cb2807966f43249ed
Joey Guerra <github@joeyguerra.com>
2025-03-23 15:56:42 -0500

    Oops

--- test/ProxyAgent.mjs
+++ test/ProxyAgent.mjs
@@ -5,7 +5,6 @@ import hubotSlackMock from '../index.mjs'
 import { loadBot, Robot } from 'hubot'
 import { SlackTextMessage, ReactionMessage, FileSharedMessage } from '../src/Message.mjs'
 import EventEmitter from 'node:events'
-import { ProxyAgent } from 'proxy-agent'
 import { WebClient } from '@slack/web-api'
 
 class SocketModeClientMock extends EventEmitter{
@@ -21,6 +20,12 @@ class SocketModeClientMock extends EventEmitter{
 class WebClientMock extends WebClient{
     constructor(token, options) {
         super(token, options)
+    }
+}
+
+class ProxyAgent {
+    constructor() {
+        this.type = 'proxy'
     }
 }
 
